a793c307584c700781a14be87580b109e84cfdf7
danielmbcn <danielm200@gmail.com>
2024-03-19 18:06:42 +0100

    Added int parameters recognition

--- topic14/drawing_language_parser_ex4.py
+++ topic14/drawing_language_parser_ex4.py
@@ -20,7 +20,9 @@ designed so that it is simple to add new commands.
 
 # We assume that the commands input is SVG-like, that is, a string in which
 # each command is separated by a space. If there are numbers, they are 
-# associated to the last letter.
+# associated to the letter they follow.
+
+import re
 
 class CustomMiniLanguage ():
 	def __init__ (self):
@@ -36,38 +38,55 @@ class CustomMiniLanguage ():
 			'D': 'pen down'
 		}
 
-	def getActionByCommand (self, command):
+		self.requires_int_parameter = ['P', 'N', 'S', 'W', 'E']
+
+	def getActionByCommand (self, command, int_parameter = None):
 		if command in self.actions:
-			return self.actions[command]
+			action = self.actions[command]
+			if (command in self.requires_int_parameter) and int_parameter:
+				action += " " + str (int_parameter)
+			return action
 		else:
-			return ""
+			return "Unknown command"
 
 	def parseUserCommands (self, commands_list):
 		commands_list = commands_list.split (" ")
+		command_verb = None
+		command_int_parameter = None
 		translated_actions = []
+		current_action = ""
 		result = ""
+
 		for i in range (0, len (commands_list)):
+			command_verb = "".join (re.findall (r'[A-Za-z]', commands_list[i]))
+			command_int_parameter = "".join (re.findall (r'\d', commands_list[i]))
+
 			if len (translated_actions) > 0 and ("draw" in translated_actions[i - 1] or "then" in translated_actions[i - 1]):
-				translated_actions.append (self.getActionByCommand (commands_list[i]).replace ("draw", "then"))
+				current_action = self.getActionByCommand (command_verb, command_int_parameter).replace ("draw", "then")
 			else:
-				translated_actions.append (self.getActionByCommand (commands_list[i]))
+				current_action = self.getActionByCommand (command_verb, command_int_parameter)
+
+			translated_actions.append (current_action)
 		
 		for action in translated_actions:
 			result += action + "\n"
 
 		return result[:-1]
 
-	def addNewCommand (self, command, action):
+	def addNewCommand (self, command, action, requires_int_parameter = False):
 		if command in self.actions:
 			print ("This command already exists and it means '" + action + "'")
 		else:
 			self.actions[command] = action
+			if requires_int_parameter:
+				self.requires_int_parameter.append (command)
 
 cml = CustomMiniLanguage ()
 
-commands_list = "P D W N E S U"
-
+commands_list = "P2 D W2 N1 E2 S1 U"
 print (cml.parseUserCommands (commands_list))
+
+#commands_list = input ("Enter commands: ")
 
 """
 cml.addNewCommand ("C", "cancel")
